d5305a72cab6e4021bc3abd59710a1ee1686a7bb
tonidy <tonidy@outlook.com>
2021-12-12 15:34:52 +0700

    Add simple code for opening tokped website

--- src/main/java/com/tonidy/scraper/App.java
+++ src/main/java/com/tonidy/scraper/App.java
@@ -1,13 +1,31 @@
 package com.tonidy.scraper;
 
+import java.text.MessageFormat;
+
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Playwright;
+
 /**
- * Hello world!
+ * Main app
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try(var playwright = Playwright.create()) 
+        {
+            var browserType = new BrowserType.LaunchOptions().setHeadless(false);
+            var browser = playwright.webkit().launch(browserType);
+            var page = browser.newPage();
+            var category = "handphone-tablet";
+            page.navigate(MessageFormat.format("https://www.tokopedia.com/p/{0}", category));
+            System.out.println(page.title());
+            //System.out.println(page.content());
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
